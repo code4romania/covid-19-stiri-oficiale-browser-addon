46a7275f66ffaf019bc0b07bd9f25851a73f7ac4
Andrei Cristian Petcu <andrei@ceata.org>
2020-03-13 21:31:01 +0200

    add more than one term

--- src/emergency_news.js
+++ src/emergency_news.js
@@ -1,12 +1,19 @@
-walk(document.body);
-tippy('.emergency_news', {
-	content: `
-	<div><b>Coronavirus</b></div>
-	<div>COVID-19 exte un virus extrem de periculos. Puteti afla mai multe pe pagina <a href="https://ncov2019.live/">ncov2019.live</a></div>`,
-	allowHTML: true,
-	interactive: true,
-	theme: 'light'
-});
+setTimeout(() => {
+	walk(document.body);
+}, 1000);
+
+const terms = {
+	"coronavirus": {
+		"title": "Coronavirus",
+		"explanation": "Coronaviruses (CoV) are a large family of viruses that cause illness ranging from the common cold to more severe diseases",
+		"link": "https://www.who.int/health-topics/coronavirus"
+	},
+	"COVID-19": {
+		"title": "Covid-19",
+		"explanation": "Coronavirus disease 2019 (COVID-19) is an infectious disease caused by the severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2). The disease has spread globally since 2019, resulting in the 2019–20 coronavirus pandemic. Common symptoms include fever, cough and shortness of breath. Muscle pain, sputum production and sore throat are some of the less common symptoms.0 While the majority of cases result in mild symptoms, some progress to pneumonia and multi-organ failure. The case fatality rate is estimated at between 1% and 5% but varies by age and other health conditions.",
+		"link": "https://en.m.wikipedia.org/wiki/Coronavirus_disease_2019"
+	}
+};
 
 function walk(node) {
 	// I stole this function from here:
@@ -40,26 +47,43 @@ function walk(node) {
 	}
 }
 
+let tooltipCount = 0;
+
 function handleText(textNode) {
-	try {
-		const term = "coronavirus";
-		const matchIndex = textNode.nodeValue.indexOf(term);
-		if (matchIndex > 0) {
-			const splitText = textNode.nodeValue.split(term);
-			const textBefore = splitText[0];
-			const textAfter = splitText[1];
+	for (var term in terms) {
+		if (terms.hasOwnProperty(term)) {
+			var termData = terms[term];
+			try {
+				const matchIndex = textNode.nodeValue.indexOf(term);
+				if (matchIndex > 0) {
+					const splitText = textNode.nodeValue.split(term);
+					const textBefore = splitText[0];
+					const textAfter = splitText[1];
 
-			const before = document.createTextNode(textBefore);
-			const after = textNode;
-			after.nodeValue = textAfter;
-			textNode.parentNode.insertBefore(before, after);
-			var divWithTooltip = document.createElement("span");
-			divWithTooltip.className = "emergency_news";
-			divWithTooltip.textContent = term;
+					const before = document.createTextNode(textBefore);
+					const after = textNode;
+					after.nodeValue = textAfter;
+					textNode.parentNode.insertBefore(before, after);
+					var divWithTooltip = document.createElement("span");
+					tooltipCount++;
+					divWithTooltip.classList.add("emergency_news");
+					divWithTooltip.classList.add("emergency_news_item" + tooltipCount);
+					divWithTooltip.textContent = term;
 
-			textNode.parentNode.insertBefore(divWithTooltip, after);
+					textNode.parentNode.insertBefore(divWithTooltip, after);
+					tippy('.emergency_news_item' +  + tooltipCount, {
+						content: `
+						<div><b>${termData.title}</b></div>
+						<div>${termData.explanation} <a href="${termData.link}">${termData.link}</a></div>`,
+						allowHTML: true,
+						interactive: true,
+						theme: 'light'
+					});
+					
+				}
+			} finally { }
 		}
-	} finally { }
+	}
 
 }
 
