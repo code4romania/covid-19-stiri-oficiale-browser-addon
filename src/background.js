f684c3bf9acb95caad40e44dfebd7e6718b731bc
Andrei Cristian Petcu <andrei@ceata.org>
2020-03-13 23:03:54 +0200

    fetch data in the background twice a day

--- src/background.js
+++ src/background.js
@@ -3,13 +3,11 @@ let terms = {};
 async function loadData() {
     const httpData = await fetch("https://raw.githubusercontent.com/code4romania/emergency-news-addon/master/terms.json");
     terms = await httpData.json();
+    setTimeout(loadData, 1000*60*60*12);
 }
 
-
-function handleMessage(request, sender, sendResponse) {
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     sendResponse({ terms });
-}
-
-browser.runtime.onMessage.addListener(handleMessage);
+});
 
 loadData();
