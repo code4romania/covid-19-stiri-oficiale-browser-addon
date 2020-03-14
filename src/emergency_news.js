a6408f14063b0ee9d2cd7b7b16773668714e9ff7
Andrei Cristian Petcu <andrei@ceata.org>
2020-03-14 22:18:44 +0200

    #9 #10 a bit of cleanup

--- src/emergency_news.js
+++ src/emergency_news.js
@@ -1,4 +1,5 @@
-let terms = {};
+
+var terms = {};
 
 browser.runtime.sendMessage({}).then((message) => {
 	terms = message.terms;
@@ -33,60 +34,64 @@ function walk(node) {
 	}
 }
 
-
 function simplifyText(input) {
 	const noComas = input.toLowerCase()
-		.replace(/ș/g, 's')
-		.replace(/ț/g, 't')
-		.replace(/ă/g, 'a')
-		.replace(/â/g, 'a')
-		.replace(/î/g, 'i')
+		.split('ș').join('s')
+		.split('ț').join('t')
+		.split('ă').join('a')
+		.split('â').join('a')
+		.split('î').join('i')
 		;
-	const noCedila = input.toLowerCase()
-		.replace(/ş/g, 's')
-		.replace(/ţ/g, 't')
+	const noCedila = noComas.toLowerCase()
+		.split('ş').join('s')
+		.split('ţ').join('t')
 		;
 	return noCedila;
 }
 
-let tooltipCount = 0;
+var tooltipCount = 0;
 function handleText(textNode) {
+	if (textNode.nodeValue.indexOf("http://") === 0 || textNode.nodeValue.indexOf("https://") === 0) {
+		return;
+	}
 	for (let term in terms) {
 		if (terms.hasOwnProperty(term)) {
 			let termData = terms[term];
-			const splittedText = splitTextByTerm(textNode.nodeValue, term);
-			if (splittedText.matchType !== "MISSING") {
-				const textBefore = splittedText.begin;
-				const textAfter = splittedText.end;
+			try {
+				const splittedText = splitTextByTerm(textNode.nodeValue, term);
+				if (splittedText.matchType !== "MISSING") {
+					const textBefore = splittedText.begin;
+					const textAfter = splittedText.end;
 
-				const before = document.createTextNode(textBefore);
-				const after = textNode;
-				after.nodeValue = textAfter;
-				textNode.parentNode.insertBefore(before, after);
-				let divWithTooltip = document.createElement("span");
-				tooltipCount++;
-				divWithTooltip.classList.add("emergency_news");
-				divWithTooltip.classList.add("emergency_news_item" + tooltipCount);
-				divWithTooltip.textContent = term;
+					const before = document.createTextNode(textBefore);
+					const after = textNode;
+					after.nodeValue = textAfter;
+					textNode.parentNode.insertBefore(before, after);
+					let divWithTooltip = document.createElement("span");
+					tooltipCount++;
+					divWithTooltip.classList.add("emergency_news");
+					divWithTooltip.classList.add("emergency_news_item" + tooltipCount);
+					divWithTooltip.textContent = term;
 
-				textNode.parentNode.insertBefore(divWithTooltip, after);
+					textNode.parentNode.insertBefore(divWithTooltip, after);
 
-				let content = `<div><b>${termData.title}</b></div>`;
-				if (termData.explanation) {
-					content += `<div>${termData.explanation}"</div>`;
+					let content = `<div><b>${termData.title}</b></div>`;
+					if (termData.explanation) {
+						content += `<div>${termData.explanation}"</div>`;
+					}
+					for (let i = 0; i < termData.links.length; i++) {
+						const link = termData.links[i];
+						content += `<div><a href="${link}">${link}</a></div>`;
+					}
+
+					tippy('.emergency_news_item' + + tooltipCount, {
+						content: content,
+						allowHTML: true,
+						interactive: true,
+						theme: 'light'
+					});
 				}
-				for (let i = 0; i < termData.links.length; i++) {
-					const link = termData.links[i];
-					content += `<div><a href="${link}">${link}</a></div>`;
-				}
-
-				tippy('.emergency_news_item' + + tooltipCount, {
-					content: content,
-					allowHTML: true,
-					interactive: true,
-					theme: 'light'
-				});
-			}
+			} catch{ }
 		}
 	}
 
@@ -102,7 +107,9 @@ function splitTextByTerm(fullString, term) {
 	let begin;
 	let end;
 	let matchType;
-	if (termIndex === 0) {
+	if (termIndex === -1) {
+		matchType = "MISSING";
+	} else if (termIndex === 0) {
 		matchType = "START";
 		begin = "";
 		end = fullString.substring(term.length, stop);
@@ -110,12 +117,10 @@ function splitTextByTerm(fullString, term) {
 		matchType = "END";
 		begin = fullString.substring(0, termIndex);
 		end = "";
-	} else if (termIndex !== -1) {
+	} else {
 		matchType = "MID";
 		begin = fullString.substring(0, termIndex);
 		end = fullString.substring(termIndex - 1, stop);
-	} else {
-		matchType = "MISSING";
 	}
 	return {
 		begin,
