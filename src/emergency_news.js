b98c069eb5e319ea7aa5b6dd815c67fe4b6b844d
Andrei Cristian Petcu <andrei@ceata.org>
2020-03-15 16:25:54 +0200

    fix #18 font size is !important

--- src/emergency_news.js
+++ src/emergency_news.js
@@ -170,7 +170,6 @@ function createTooltip(termData, tooltipCount) {
         content: content,
         allowHTML: true,
         interactive: true,
-        trigger: 'click',
         theme: 'light'
     });
 }
