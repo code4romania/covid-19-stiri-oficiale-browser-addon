0b7e02c4d8367280586dc0f41afc60fc3273317d
Andrei Cristian Petcu <andrei@ceata.org>
2020-03-17 20:14:51 +0200

    #32 auto load js in all pages for chrome, just like Firefox

--- src/background.js
+++ src/background.js
@@ -46,37 +46,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 loadData();
-
-if (!!chrome.contextMenus) {
-
-    chrome.contextMenus.create({
-        id: "highlight-terms",
-        title: "Evidențiează surse oficiale",
-        contexts: ["all"]
-    }, () => {});
-
-    chrome.contextMenus.onClicked.addListener(function(info, tab) {
-        if (info.menuItemId == "highlight-terms") {
-            const styles = [
-                "dependencies/light.css",
-                "emergency_news.css",
-            ];
-            styles.forEach((stylesName) => {
-                chrome.tabs.insertCSS(tab.id, {
-                    file: stylesName,
-                }, () => {});
-            })
-            const scripts = [
-                "dependencies/browser-polyfill.js",
-                "dependencies/popper.js",
-                "dependencies/tippy-bundle.umd.js",
-                "emergency_news.js"
-            ];
-            scripts.forEach((scriptName) => {
-                chrome.tabs.executeScript(tab.id, {
-                    file: scriptName,
-                }, () => {});
-            })
-        }
-    });
-}
