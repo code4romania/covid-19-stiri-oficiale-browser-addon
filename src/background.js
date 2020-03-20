addb175095f39d4f63e20e9b60367cbc1a92373c
Andrei Cristian Petcu <andrei@ceata.org>
2020-03-20 20:19:44 +0200

    #35 use new format

--- src/background.js
+++ src/background.js
@@ -1,48 +1,39 @@
-let terms = {};
+let config = {};
 
 async function loadData() {
-    let termsLocation = './terms.json';
+    let configLocation = './config.json';
     const isFirefoxProd = browser.runtime.id === "{2164fef6-64f4-4a8b-9a6d-9dd9c500dd88}";
     const isChromeProd = browser.runtime.id === "pdcpkplohipjhdfdchpmgekifmcdbnha";
     if (isFirefoxProd || isChromeProd) {
-        termsLocation = 'https://raw.githubusercontent.com/code4romania/emergency-news-addon/master/src/terms.json';
+        configLocation = 'https://raw.githubusercontent.com/code4romania/emergency-news-addon/master/src/config.json';
     }
-    const httpData = await fetch(termsLocation);
-    terms = expandTerms(await httpData.json());
+    const httpData = await fetch(configLocation);
+    config = await httpData.json();
+    config.terms = expandTerms(config.terms);
     setTimeout(loadData, 1000 * 60 * 60);
 }
 
 function expandTerms(termsInput) {
-    let newTerms = {};
-    for (let term in termsInput) {
-        if (termsInput.hasOwnProperty(term)) {
-            newTerms[term] = termsInput[term];
-            if (termsInput[term].hasOwnProperty("aliases")) {
-                let aliases = termsInput[term].aliases;
-                aliases.forEach(function(alias) {
-                    newTerms[alias] = termsInput[term];
+    let newTerms = [];
+    termsInput.forEach((term) => {
+        if (!!term.aliases) {
+            term.aliases.forEach(function (alias) {
+                newTerms.push({
+                    "key": alias,
+                    "value": term
                 });
-            }
-        }
-    }
-    let orderedTerms = [];
-    Object.keys(newTerms)
-        .sort((a, b) => {
-            return b.length - a.length || b.localeCompare(a);
-        })
-        .forEach(function(key) {
-            orderedTerms.push({
-                "key": key,
-                "value": newTerms[key]
             });
-        });
-    return orderedTerms;
+        }
+    });
+    newTerms.sort((a, b) => {
+        return b.key.length - a.key.length || b.key.localeCompare(a.key);
+    })
+    return newTerms;
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    sendResponse({
-        "terms": terms
-    });
+    console.log(config);
+    sendResponse(config);
 });
 
 loadData();
