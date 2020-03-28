5956405560fd9496cfb898312799a8e68119f2f9
Andrei Cristian Petcu <andrei@ceata.org>
2020-03-28 05:09:59 +0200

    #36 added android configs

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,16 +1,23 @@
 const args = require('./lib/args');
-const fs = require('fs-extra')
+const fs = require('fs-extra');
+
+const manifestFiles = {
+    'firefox': 'manifest_firefox.json',
+    'android': 'manifest_firefox.json',
+    'chrome': 'manifest_chrome.json'
+};
 
 function copy(cb) {
   fs.ensureDirSync(`dist/${args.vendor}/dependencies/`);
   fs.copySync('node_modules/@popperjs/core/dist/umd/popper.js', `dist/${args.vendor}/dependencies/popper.js`);
   fs.copySync('node_modules/tippy.js/dist/tippy-bundle.umd.js', `dist/${args.vendor}/dependencies/tippy-bundle.umd.js`);
   fs.copySync('node_modules/tippy.js/themes/light.css', `dist/${args.vendor}/dependencies/light.css`);
-  if(args.vendor === 'chrome'){
+  if (args.vendor === 'chrome') {
     fs.copySync('node_modules/webextension-polyfill/dist/browser-polyfill.js', `dist/${args.vendor}/dependencies/browser-polyfill.js`);
   }
   fs.copySync('src', `dist/${args.vendor}/`);
-  fs.copySync(`manifest_${args.vendor}.json`, `dist/${args.vendor}/manifest.json`);
+  const manifest = manifestFiles[args.vendor];
+  fs.copySync(`${manifest}`, `dist/${args.vendor}/manifest.json`);
   cb();
 }
 
