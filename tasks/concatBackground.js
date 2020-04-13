fcd614ea03279e8a7c107cdbd1ac4009c4f41d62
Andrei Cristian Petcu <andrei@ceata.org>
2020-04-13 21:53:15 +0300

    added file names

--- tasks/concatBackground.js
+++ tasks/concatBackground.js
@@ -1,9 +1,13 @@
 const args = require('./lib/args');
 const { src, dest } = require('gulp');
 const concat = require('gulp-concat');
+var insert = require('gulp-insert');
 
 function concatBackground(cb) {
   src('src/background/**/*.js')
+    .pipe(insert.transform(function (contents, file) {
+      return `//------------------------------\n// ${file.path.split("src/")[1]}\n//------------------------------\n\n${contents}\n`;
+    }))
     .pipe(concat('background.js'))
     .pipe(dest(`dist/${args.vendor}`));
   if (cb) {
